0b984af51c5928b145bb1bc9f1eb108379aaf007
mubonassi <115490684+mubonassi@users.noreply.github.com>
2025-05-23 11:55:19 -0300

    Delete Sex 14h30 - Lappy Xird directory

--- Sex 14h30 - Lappy Xird/Scripts/gameover.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_btn_retry_pressed():
-	get_tree().reload_current_scene()
-
-func _on_btn_menu_pressed():
-	global.partida = false
-	get_tree().reload_current_scene()
--- Sex 14h30 - Lappy Xird/Scripts/menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_btn_play_pressed():
-	global.partida = true
-	get_tree().reload_current_scene()
-
-func _on_btn_quit_pressed():
-	get_tree().quit()
